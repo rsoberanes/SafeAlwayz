408e90669696ef70b004b513eb8fce8bcb3a93bf
Key Son <keykson@yahoo.com>
2020-10-17 19:04:41 -0400

    Select Device UI created

---
+++ app/src/main/java/com/studios/primitive/safealwayz/ui/main/SelectDeviceActivity.java
@@ -0,0 +1,16 @@
+package com.studios.primitive.safealwayz.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.studios.primitive.safealwayz.R;
+
+public class SelectDeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_device);
+    }
+}
